d4530d664dc4faf9fb064b2e5cf25546f4f5c264
KrisNathan <kristophernathanael2005@gmail.com>
2026-03-19 18:04:29 +0700

    refactor: remove commented-out code from main function

--- linux-touchpad-gesture/src/main.rs
+++ linux-touchpad-gesture/src/main.rs
@@ -14,70 +14,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     touchpad_service.init_debug();
 
-    // Ok(())
-
     loop {
         touchpad_service.fetch_events()?;
     }
-
-    // let mut devices = get_touchpad_devices();
-    // if devices.is_empty() {
-    //     println!("No touchpad devices found.");
-    //     return Ok(());
-    // }
-
-    // let mut device = devices.remove(0);
-    // println!("Using touchpad: {}", device.name().unwrap_or("Unknown"));
-
-    // let bounds = get_touchpad_bounds(&device)?;
-
-    // print!(
-    //     "Touchpad bounds: X[{}, {}], Y[{}, {}]\n",
-    //     bounds.min_x, bounds.max_x, bounds.min_y, bounds.max_y
-    // );
-
-    // evdev::enumerate().for_each(|(path, device)| {
-    //     println!("{}: {}", path.display(), device.name().unwrap_or("Unknown"));
-    // });
-
-    // let f = File::open("/dev/input/event4")?;
-    // let fd = OwnedFd::from(f);
-    // let mut device = Device::from_fd(fd)?;
-
-    // let has_touchpad = check_touchpad(&device);
-    // if has_touchpad {
-    //     println!("Touchpad detected: {}", device.name().unwrap_or("Unknown"));
-    // } else {
-    //     println!("No touchpad detected.");
-    //     return Ok(());
-    // }
-
-    // loop {
-    //     for event in device.fetch_events()? {
-    //         match event.destructure() {
-    //             EventSummary::AbsoluteAxis(_, AbsoluteAxisCode::ABS_X, x) => {
-    //                 println!("X position: {}", x);
-    //             }
-    //             EventSummary::AbsoluteAxis(_, AbsoluteAxisCode::ABS_Y, y) => {
-    //                 println!("Y position: {}", y);
-    //             }
-    //             EventSummary::AbsoluteAxis(_, AbsoluteAxisCode::ABS_MT_TRACKING_ID, id) => {
-    //                 if id == -1 {
-    //                     println!("Touch ended");
-    //                 } else {
-    //                     println!("Touch started: {}", id);
-    //                 }
-    //             }
-    //             _ => {}
-    //         }
-    //     }
-    // }
-
-    // let device = Device::open("/dev/input/event2")?;
-    // check if the device has an ENTER key
-    // if device.supported_keys().map_or(false, |keys| keys.contains(KeyCode::KEY_ENTER)) {
-    //     println!("are you prepared to ENTER the world of evdev?");
-    // } else {
-    //     println!(":(");
-    // }
 }
